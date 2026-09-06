517e939d3e2022e565714dfbde7d7e993f99ba99
Hatouma Diawara <hatoumatiefingd@gmail.com>
2026-09-06 05:35:21 +0200

    Correction HTTPS Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,22 +1,26 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
+use Symfony\Component\HttpFoundation\Request;
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware): void {
-       
-   $middleware->alias([
-       'role' => \App\Http\Middleware\RoleMiddleware::class,
-   ]);
-})
-    
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+   ->withRouting(
+       web: __DIR__.'/../routes/web.php',
+       commands: __DIR__.'/../routes/console.php',
+       health: '/up',
+   )
+   ->withMiddleware(function (Middleware $middleware): void {
+       // Railway agit comme un proxy HTTPS devant Laravel.
+       // On fait confiance aux en-têtes transmis par Railway.
+       $middleware->trustProxies(
+           at: '*',
+           headers:
+               Request::HEADER_X_FORWARDED_FOR |
+               Request::HEADER_X_FORWARDED_HOST |
+               Request::HEADER_X_FORWARDED_PORT |
+               Request::HEADER_X_FORWARDED_PROTO
+       );
+   })
+   ->withExceptions(function (Exceptions $exceptions): void {
+       //
+   })->create();
