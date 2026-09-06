46c5b3b5c10965843da90a5d9384642e18d34fd9
Hatouma Diawara <hatoumatiefingd@gmail.com>
2026-09-06 05:42:39 +0200

    correction HTTPS formulaires

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,27 @@
 <?php
-
 namespace App\Providers;
-
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+   /**
+    * Enregistre les services de l'application.
+    */
+   public function register(): void
+   {
+       //
+   }
+   /**
+    * Configure l'application au démarrage.
+    */
+   public function boot(): void
+   {
+       /*
+        * En production, force Laravel à générer
+        * toutes les URLs avec HTTPS.
+        */
+       if (app()->environment('production')) {
+           URL::forceScheme('https');
+       }
+   }
 }
