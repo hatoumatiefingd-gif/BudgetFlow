e7aa659ae734dfc17e9cfd64ab9153b7643e4b57
Hatouma Diawara <hatoumatiefingd@gmail.com>
2026-09-16 14:57:39 +0200

    Responsive mobile espace utilisateur

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         {{-- CSS personnalisé de BudgetFlow --}}
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('css/mobile.css') }}?v=2">
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
